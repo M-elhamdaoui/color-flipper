80ac1862a296c31eb936a56811913ab308c0291f
melhamdaoui811 <melhamdaoui811@gmail.com>
2020-11-08 18:47:46 +0100

    Simple color

---
+++ dist/js/script.js
@@ -0,0 +1,27 @@
+const colors =["INDIANRED","RED","SALMON","DARKSALMON","PINK","LIGHTPINK","HOTPINK","GOLD","YELLOW","PURPLE","GREEN","CYAN","SKYBLUE","LIGHTSKYBLUE","BLUE"];
+ console.log(parseInt(getRandom()*colors.length));
+ const btn1 = document.querySelector("#btnS");
+ const btn2 = document.querySelector("#btnH");
+ const colorContainer =document.querySelector(".color");
+ const color =document.querySelector("#color");
+ let a =255;
+ console.log(a.toString(16));
+
+
+  btn1.addEventListener("click",function(){
+      let co =colors[parseInt(getRandom()*(colors.length-1))];
+    colorContainer.style.backgroundColor=co;
+    color.innerHTML=co;
+  })
+
+
+
+
+
+
+
+
+
+function getRandom(){
+    return Math.random();
+}
